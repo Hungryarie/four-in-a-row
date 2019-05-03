2138a850fbb4f5606b0e3387f5145fb9ac85dc1d
Hungryarie <arnoudzwakenberg@gmail.com>
2019-05-03 23:09:50 +0200

    diagnal checks added. And showfield updated

--- fiar.py
+++ fiar.py
@@ -1,29 +1,56 @@
 import numpy as np 
 import random
 
+
 def main():
     print ("Starting game")
     #PlayerName = input('Player name:')
     PlayerName="Arnoud"
     print (f"playername: {PlayerName}")
     Game = StartGame(PlayerName)
+    Game.ShowField()
 
     while True:
+        
+
         if Game.nextTurn==1:
-            print (f"{Game.playerName}'s turn")
+            print (f"{Game.playerColor}:{Game.playerName}'s turn")
             
             ColumnNo= int(input('column no:'))
 
             Game.addCoin(ColumnNo,Game.nextTurn)
-        if Game.nextTurn==2:
-            print (f"{Game.opponentName}'s turn")
+            Game.ShowField2()
 
-            Game.addCoin(random.randint(0,Game.columns-1),Game.nextTurn)
-        
-        Game.ShowField()
         if Game.checkForWinner()!=0:
             break
 
+        if Game.nextTurn==2:
+            print (f"{Game.opponentColor}:{Game.opponentName}'s turn")
+
+            Game.addCoin(random.randint(0,Game.columns-1),Game.nextTurn)
+            Game.ShowField2()
+        
+        if Game.checkForWinner()!=0:
+            break
+
+
+def NProtate45(array):
+    # Rotate numpy array 45 degrees
+    rows, cols = array.shape
+    rot = np.zeros([rows,cols+rows-1],dtype=int)
+    for i in range(rows):
+        for j in range(cols):
+            rot[i,i + j] = array[i,j]
+    return rot
+
+def NProtate275(array):
+    # Rotate numpy array 275 degrees
+    rows, cols = array.shape
+    rot = np.zeros([rows,cols+rows-1],dtype=int)
+    for i in range(rows):
+        for j in range(cols):
+            rot[i,i - j] = array[i,j]
+    return rot
 
 def checkFourOnARow( x ):
     #print ("x:", x)
@@ -60,6 +87,7 @@ class StartGame:
         self.nextTurn=random.randint(1,2)
         self.playingField= np.zeros([self.rows,self.columns], dtype=int)
         self.winner=0                   # winner id. 0 is no winner yet
+        self.winnerhow="none"
         self.counts=0                   # amount of tries before winning
 
     def checkForWinner(self):
@@ -67,27 +95,36 @@ class StartGame:
         if self.winner==0:
             # check for horizontal winner
             self.winner=self.checkForWinnerHor()
+            self.winnerhow="Horizontal"
         if self.winner==0:
             # no winner? check for vertical winner
             self.winner=self.checkForWinnerVer()
+            self.winnerhow="Vertical"
         if self.winner==0:
-            # no winner? check for diagnal winner
-            # 2do adding diagnal check
-            self.checkForWinnerDiaRight()
-            pass
+            # no winner? check for diagnal winner Right
+            self.winner=self.checkForWinnerDiaRight()
+            self.winnerhow="Diagnal Right"
+        if self.winner==0:
+            # no winner? check for diagnal winner Right
+            self.winner=self.checkForWinnerDiaLeft()
+            self.winnerhow="Diagnal Left"
         if self.winner!=0:
-            print (f"winner:{self.winner}")
+            print (f"winner:{self.winner}, how:{self.winnerhow}")
         else:
             print ("no winner yet")
 
         return self.winner
 
     def checkForWinnerDiaRight (self):
-        #2do adding a 0 to row 0, adding 2 zeros to row 1 etc.
-        rows, cols =self.playingField.shape
+        print("Check for a Diagnal Right Winner")
+        array= NProtate45(self.playingField) # scew the playingfield 45degrees 
+        return sum(np.apply_along_axis( checkFourOnARow, axis=0, arr=array )) #axis=0: vertical
 
-        return sum(np.apply_along_axis( checkFourOnARow, axis=0, arr=self.playingField )) #axis=0: vertical
-        
+    def checkForWinnerDiaLeft (self):
+        print("Check for a Diagnal Left Winner")
+        array= NProtate275(self.playingField) # scew the playingfield minus 45degrees 
+        return sum(np.apply_along_axis( checkFourOnARow, axis=0, arr=array )) #axis=0: vertical       
+
     def checkForWinnerHor (self): #, WhosTurn):
         print("Check for a Horizontal Winner")
         #print (np.apply_along_axis( checkFourOnARow, axis=1, arr=self.playingField ))
@@ -124,7 +161,7 @@ class StartGame:
         
         if success:
             # Set the next turn
-            print (f"current player={self.nextTurn}. next = {abs(self.nextTurn -2)+1}")
+            #print (f"current player={self.nextTurn}. next = {abs(self.nextTurn -2)+1}")
             self.nextTurn=abs(self.nextTurn -2)+1
             # iterate the number of tries
             self.counts +=1
@@ -132,6 +169,21 @@ class StartGame:
     def ShowField(self):
         print (" |0|1|2|3|4|5|6| << colums")
         print (self.playingField)
+    
+    def ShowField2(self):
+        print ("|0|1|2|3|4|5|6| << colums")
+        for i in self.playingField:
+            row=""
+            for j in i:
+                if j==0:
+                    j=" "
+                elif j==1:
+                    j=self.playerColor
+                elif j==2:
+                    j=self.opponentColor
+                row+="|"+str(j)   
+            row+="|"       
+            print (row)
 
 if __name__ == '__main__':
     main()
