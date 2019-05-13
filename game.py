87bad2aa6c262dc8877ec4e92bcf84271c25a23a
Hungryarie <arnoudzwakenberg@gmail.com>
2019-05-13 20:40:43 +0200

    removed if __name__ == '__main__':

--- game.py
+++ game.py
@@ -2,28 +2,6 @@ import numpy as np
 import random
 
 
-def main():
-    print ("Starting game")
-    #PlayerName = input('Player name:')
-    PlayerName="Arnoud"
-    print (f"playername: {PlayerName}")
-    Game = FiarGame(PlayerName)
-    Game.ShowField2()
-
-    while True:
-        if Game.nextTurn==1:
-            print (f"{Game.playerColor}:{Game.playerName}'s turn")
-            ColumnNo= input('column no:')
-        if Game.nextTurn==2:
-            print (f"{Game.opponentColor}:{Game.opponentName}'s turn")
-            ColumnNo=random.randint(0,Game.columns-1)
-
-        if Game.addCoin(ColumnNo,Game.nextTurn):
-            Game.ShowField2()
-            if Game.checkForWinner()!=0:
-                break
-
-
 def NProtate45(array):
     # Rotate numpy array 45 degrees
     rows, cols = array.shape
@@ -192,6 +170,3 @@ class FiarGame:
                 row+="|"+str(j)   
             row+="|"       
             print (row)
-
-if __name__ == '__main__':
-    main()
