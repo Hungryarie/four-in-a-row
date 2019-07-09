92ba27a96026fd10a3380bdf8a2394c6d14b8e38
Hungryarie <arnoudzwakenberg@gmail.com>
2019-07-09 22:00:08 +0200

    added a logging class. changed model_1

--- model.py
+++ model.py
@@ -1,11 +1,55 @@
 from keras.callbacks import TensorBoard
 import tensorflow as tf
 
-from keras.models import Sequential, load_model
+from keras.models import Sequential, load_model  # , clone_model
 from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten
 from keras.optimizers import Adam
 
 from constants import *
+from game import FiarGame
+import time
+
+from datetime import datetime
+
+
+class ModelLog():
+    def __init__(self):
+        pass
+
+    def add_model_info(self, model):
+        self.model_name = model.model_name
+        self.model_starttime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.model_timestamp = model.timestamp
+
+    def add_constants(self):
+        self.constants = {
+            "MIN_REWARD": MIN_REWARD,
+            "EPISODES": EPISODES,
+            "REWARD_WINNING": FiarGame.REWARD_WINNING,
+            "REWARD_LOSING": FiarGame.REWARD_LOSING,
+            "REWARD_TIE": FiarGame.REWARD_TIE,
+            "REWARD_INVALID_MOVE": FiarGame.REWARD_INVALID_MOVE,
+            "DISCOUNT": DISCOUNT,
+            "REPLAY_MEMORY_SIZE": REPLAY_MEMORY_SIZE,
+            "MIN_REPLAY_MEMORY_SIZE": MIN_REPLAY_MEMORY_SIZE,
+            "MINIBATCH_SIZE": MINIBATCH_SIZE,
+            "UPDATE_TARGET_EVERY": UPDATE_TARGET_EVERY,
+            "MEMORY_FRACTION": MEMORY_FRACTION,
+            "epsilon": epsilon,
+            "EPSILON_DECAY": EPSILON_DECAY,
+            "MIN_EPSILON": MIN_EPSILON,
+            "AGGREGATE_STATS_EVERY": AGGREGATE_STATS_EVERY,
+            "SHOW_PREVIEW": SHOW_PREVIEW,
+            "LOAD_MODEL": LOAD_MODEL
+        }
+
+    def write_to_file(self, path):
+        f = open(path, "a+")
+        f.write(f"{self.model_starttime} \nmodel name = {self.model_name}-{self.model_timestamp}\n")
+        for key, constant in self.constants.items():
+            f.write(f"{key} = {constant}\n")
+        f.write("\n")
+        f.close()
 
 
 # Own Tensorboard class
@@ -41,10 +85,18 @@ class ModifiedTensorBoard(TensorBoard):
         self._write_logs(stats, self.step)
 
 
+#class load_a_model:
+#    def __init__(self, path):
+#        self.model = load_model(path)
+#        self.target_model = load_model(path)
+
+
 class model_1:
     def __init__(self, input_shape, output_num):
         self.model = self.create_model(input_shape, output_num)
         self.target_model = self.create_model(input_shape, output_num)
+        self.model_name = 'model1_conv2x128'
+        self.timestamp = int(time.time())
 
     def create_model(self, input_shape, output_num):
         if LOAD_MODEL is not None:
@@ -54,14 +106,14 @@ class model_1:
         else:
             model = Sequential()
 
-            model.add(Conv2D(256, (3, 3), input_shape=input_shape))  # OBSERVATION_SPACE_VALUES = (10, 10, 3) a 10x10 RGB image.
+            model.add(Conv2D(128, (3, 3), input_shape=input_shape, data_format="channels_last", padding='same'))  # OBSERVATION_SPACE_VALUES = (10, 10, 3) a 10x10 RGB image.
             model.add(Activation('relu'))
-            model.add(MaxPooling2D(pool_size=(2, 2)))
+            #model.add(MaxPooling2D(pool_size=(2, 2)))
             model.add(Dropout(0.2))
 
-            model.add(Conv2D(256, (3, 3)))
+            model.add(Conv2D(128, (3, 3), padding='same'))
             model.add(Activation('relu'))
-            model.add(MaxPooling2D(pool_size=(2, 2)))
+            #model.add(MaxPooling2D(pool_size=(2, 2)))
             model.add(Dropout(0.2))
 
             model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
@@ -76,6 +128,8 @@ class model_2:
     def __init__(self, input_shape, output_num):
         self.model = self.create_model(input_shape, output_num)
         self.target_model = self.create_model(input_shape, output_num)
+        self.model_name = 'model2_dense1x64'
+        self.timestamp = int(time.time())
 
     def create_model(self, input_shape, output_num):
         # just for testing a quicker model
