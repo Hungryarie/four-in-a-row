0b208ae7e2bbc25c189b94e40bbcd686178fb5d7
Hungryarie <arnoudzwakenberg@gmail.com>
2019-07-12 14:51:20 +0200

    minor change

--- constants.py
+++ constants.py
@@ -1,12 +1,11 @@
 # Constants
 DISCOUNT = 0.99
-REPLAY_MEMORY_SIZE = 50_000  # How many last steps to keep for model training
+REPLAY_MEMORY_SIZE = 25_000  # 50_000  # How many last steps to keep for model training
 MIN_REPLAY_MEMORY_SIZE = 1_000  # Minimum number of steps in a memory to start training
 MINIBATCH_SIZE = 64  # How many steps (samples) to use for training
-UPDATE_TARGET_EVERY = 5  # Terminal states (end of episodes)
-MODEL_NAME = '2x256-2p-inc-fix(new rew3)'
-MIN_REWARD = 10  # For model save
-MEMORY_FRACTION = 0.20
+UPDATE_TARGET_EVERY = 10# 5  # Terminal states (end of episodes)
+MIN_REWARD = 5.5  # For model save
+MEMORY_FRACTION = 0.20 # not so relevant yet
 
 # Environment settings
 EPISODES = 10_000
