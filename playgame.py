7e1ec07020a40d20c73d4f50aef1d43f81a0512f
Hungryarie <arnoudzwakenberg@gmail.com>
2019-09-09 21:43:15 +0200

    multiple changes: -added batch training -after training delete objects -added descr. to tensorboard -added winner how to tensorboard (hor/ver/diag)

--- playgame.py
+++ playgame.py
@@ -5,7 +5,7 @@ import players
 from game import FiarGame
 from env import enviroment
 #
-from model import ModelLog, load_a_model, model1, model2, model3, model4
+from model import ModelLog, load_a_model, model1, model2, model3, model4, model4a, model4b, model4catcross, model5
 from tqdm import tqdm
 from constants import *
 import os
@@ -17,7 +17,7 @@ from datetime import datetime
 SHOW_EVERY = 50
 
 
-def trainNN():
+def trainNN(Model=None):
     # For stats
     ep_rewards = [-20]
     epsilon = 1
@@ -31,15 +31,17 @@ def trainNN():
     if not os.path.isdir('models'):
         os.makedirs('models')
 
-    Model = model4(input_shape=(6, 7, 1), output_num=7)  # (7, 6, 1)(1, 42)
-    #Model = load_a_model('models\dense2x128(softmax)_startstamp1567090369_episode9050__170.00max__152.60avg___95.00min__1567092303.model')
-    #Model2 = load_a_model('models\dense2x128(softmax)_startstamp1567090369_episode9050__170.00max__152.60avg___95.00min__1567092303.model')
-    Model2 = load_a_model('models\model4_dense2x128(softmax)_startstamp1567415644_endtraining__130.00max___-7.30avg_-430.00min_1567418542.model')
+    if Model is None:
+        Model = model5(input_shape=(6, 7, 1), output_num=7)  # (7, 6, 1)(1, 42)
+        #Model = load_a_model('models\dense2x128(softmax)_startstamp1567090369_episode9050__170.00max__152.60avg___95.00min__1567092303.model')
+        #Model2 = load_a_model('models\dense2x128(softmax)_startstamp1567090369_episode9050__170.00max__152.60avg___95.00min__1567092303.model')
+    #Model2 = load_a_model('models\model4a_dense2x128(softmax)(flattenLAST,input_shape bug gone lr=0.001)_startstamp1568020820_episode8350__170.00max__141.60avg__-45.00min_1568027932.model')
+
     p1 = players.DDQNPlayer(Model)
-    #p2 = players.Drunk()
-    p2 = players.DDQNPlayer(Model2)
+    p2 = players.Drunk()
+    #p2 = players.DDQNPlayer(Model2)
     p1.name = "DDQN on training"
-    p2.name = "DDQN trained model startstamp1567415644"
+    p2.name = "drunk"
     env = enviroment(p1, p2)
 
 
@@ -53,9 +55,19 @@ def trainNN():
     loose_count = 0
     draw_count = 0
     invalidmove_count = 0
+    turns_count = 0
+    count_horizontal = 0
+    count_vertical = 0
+    count_dia_right = 0
+    count_dia_left = 0
 
     log.log_text_to_file(f"start training at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
-    env.player1.setup_for_training()
+
+    # setup for training
+    p2modclass = log.model2_class
+    p2modclass.replace('/', '')
+    env.player1.setup_for_training(description=f"vs p2={log.player2_class}@{p2modclass}")
+
     # Iterate over episodes
     for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'):
 
@@ -126,23 +138,43 @@ def trainNN():
             loose_count += 1
         else:
             draw_count += 1
+        turns_count += env.turns
+        if env.winnerhow == "Horizontal":
+            count_horizontal += 1
+        if env.winnerhow == "Vertical":
+            count_vertical += 1
+        if env.winnerhow == "Diagnal Right":
+            count_dia_right += 1
+        if env.winnerhow == "Diagnal Left":
+            count_dia_left += 1
+
 
         # Append episode reward to a list and log stats (every given number of episodes)
         ep_rewards.append(episode_reward)
         if not episode % AGGREGATE_STATS_EVERY or episode == 1:
             win_ratio = win_count / (win_count + loose_count)
+            turns_count = turns_count / AGGREGATE_STATS_EVERY
+            count_horizontal = count_horizontal / AGGREGATE_STATS_EVERY
+            count_vertical = count_vertical / AGGREGATE_STATS_EVERY
+            count_dia_left = count_dia_left / AGGREGATE_STATS_EVERY
+            count_dia_right = count_dia_right / AGGREGATE_STATS_EVERY
             average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:]) / len(ep_rewards[-AGGREGATE_STATS_EVERY:])
             min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
             max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
             env.player1.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon,
                                                  win_count=win_count, loose_count=loose_count, draw_count=draw_count, invalidmove_count=invalidmove_count,
-                                                 win_ratio=win_ratio)
+                                                 win_ratio=win_ratio, turns_count=turns_count, count_horizontal=count_horizontal,
+                                                 count_vertical=count_vertical, count_dia_left=count_dia_left, count_dia_right=count_dia_right)
 
             # reset stats
             win_count = 0
             loose_count = 0
             draw_count = 0
             invalidmove_count = 0
+            count_horizontal = 0
+            count_vertical = 0
+            count_dia_left = 0
+            count_dia_right = 0
 
             # Save model, but only when avg reward is greater or equal a set value
             if average_reward >= MIN_REWARD:
@@ -163,19 +195,26 @@ def trainNN():
     log.log_text_to_file(f' {model_temp_name}\n')
     log.log_text_to_file(f"end training at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
 
+    # remove objects
+    del p1
+    del p2
+    del env
+    del log
+
 
 def PlayInEnv():
+    print("play in enviroment\n")
     #Model = load_a_model('models/PreLoadedModel_model4_dense2x128(softmax)_startstamp1563365714_endtraining_startstamp1563370404_episode7050____7.00max____5.23avg__-14.00min__1563372123.model')
     #Model = load_a_model('models\dense2x128(softmax)_startstamp1567090369_episode9050__170.00max__152.60avg___95.00min__1567092303.model')
     #Model = load_a_model('models\model4_dense2x128(softmax)_startstamp1567106898_endtraining__155.00max___19.30avg_-215.00min_1567109405.model')
-    Model = load_a_model('models\model4_dense2x128(softmax)_startstamp1567423591_episode12450__155.00max__145.30avg__100.00min_1567426853.model')
-    Model2 = load_a_model('models\dense2x128(softmax)_startstamp1567090369_episode9050__170.00max__152.60avg___95.00min__1567092303.model')
+    Model = load_a_model('models\model4catcross_dense2x128(softmax+CatCrossEntr)_startstamp1568050818_episode9600__165.00max__136.40avg__-50.00min_1568052142.model')
+    Model2 = load_a_model('models\model4a_dense2x128(softmax)(flattenLAST,input_shape bug gone lr=0.001)_startstamp1568020820_episode8350__170.00max__141.60avg__-45.00min_1568027932.model')
     p1 = players.DDQNPlayer(Model)
     #p2 = players.Human()
     p2 = players.DDQNPlayer(Model)
 
     p1.name = "DDQN"
-    p2.name = "dd"
+    p2.name = "arnoud"
     env = enviroment(p1, p2)
     env.env_info()
 
@@ -187,15 +226,15 @@ def PlayInEnv():
 def TestInEnv():
     #Model = load_a_model('models\PreLoadedModel_model4_dense2x128(softmax)_startstamp1563365714_endtraining_startstamp1563370404_episode7050____7.00max____5.23avg__-14.00min__1563372123.model')
     #Model = load_a_model('models\dense2x128(softmax)_startstamp1567090369_episode9050__170.00max__152.60avg___95.00min__1567092303.model')
-    Model = load_a_model('models\model4_dense2x128(softmax)_startstamp1567106898_endtraining__155.00max___19.30avg_-215.00min_1567109405.model')
-    Model2 = load_a_model('models\model4_dense2x128(softmax)_startstamp1567106898_endtraining__155.00max___19.30avg_-215.00min_1567109405.model')
+    Model = load_a_model('models\model4catcross_dense2x128(softmax+CatCrossEntr)_startstamp1568050818_episode9600__165.00max__136.40avg__-50.00min_1568052142.model')
+    Model2 = load_a_model('models\model4a_dense2x128(softmax)(flattenLAST,input_shape bug gone lr=0.001)_startstamp1568020820_episode8350__170.00max__141.60avg__-45.00min_1568027932.model')
     p1 = players.DDQNPlayer(Model)
     #p1 = players.Drunk()
     #p2 = players.Drunk()
     p2 = players.DDQNPlayer(Model2)
 
-    p1.name = "DDQN"
-    p2.name = "drunken"
+    p1.name = "trained against model"
+    p2.name = "trained againt random"
     env = enviroment(p1, p2)
     env.env_info()
 
@@ -220,8 +259,17 @@ def TestInEnv():
     plt.show()
 
 
+def batch_train():
+    model_list = []
+    #model_list.append(model4catcross(input_shape=(6, 7, 1), output_num=7))
+    model_list.append(model5(input_shape=(6, 7, 1), output_num=7))
+    model_list.append(model5(input_shape=(6, 7, 1), output_num=7))
+
+    for model in model_list:
+        trainNN(model)
+
 if __name__ == '__main__':
 
-    TestInEnv()
+    #TestInEnv()
     #PlayInEnv()
-    #trainNN()
+    batch_train()
