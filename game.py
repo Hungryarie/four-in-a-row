3c75bdbbaa28313f49bc0de52d83d5ff7c2641a5
Hungryarie <arnoudzwakenberg@gmail.com>
2019-11-18 22:01:13 +0100

    added a extra toprow which does nothing for the actual game itself. Only for when a invalid move has been played. then it is possible to add the move to that toprow. So you can uses that to train on the invalid move (otherwisse) state and next_state are the same. - changed stuff in the code to help get that to work

--- game.py
+++ game.py
@@ -27,9 +27,10 @@ class FiarGame:
         self.reset()
 
     def reset(self):
-        self.playingField = np.zeros([self.rows, self.columns], dtype=int)
+        self._extra_toprows = 1
+        self.playingField = np.zeros([self.rows + self._extra_toprows, self.columns], dtype=int)
         self.playingField = self.playingField[:, :, np.newaxis]
-        self.featuremap = np.zeros([self.rows, self.columns, 4], dtype=int)
+        self.featuremap = np.zeros([self.rows + self._extra_toprows, self.columns, 4], dtype=int)
         self.winner = 0                       # winner id. 0 is no winner yet
         self.winnerhow = "none"
         self.done = False
@@ -38,7 +39,7 @@ class FiarGame:
         self.current_player_value = self.getPlayerById(self.current_player).value
         self._invalid_move_played = False
         self._invalid_move_count = 0
-        self._invalid_move_action = False
+        self._invalid_move_action = None  # False
         self.prev_invalid_move_count = 0        # for collecting the max in a row invalidmove count
         self.prev_invalid_move_reset = True
 
@@ -53,36 +54,41 @@ class FiarGame:
 
     def _feature_space_field(self):
         # layer 0 = the playingField
+        layer = 0
         self.featuremap_dict['0'] = "the playingField"
-        self.featuremap[:, :, 0] = self.playingField[:, :, 0]
+        self.featuremap[:, :, layer] = self.playingField[:, :, 0]
 
     def _feature_space_active_player(self):
         # layer 1 = active player
+        layer = 1
         self.featuremap_dict['1'] = "active player"
-        active_arr = np.full((self.rows, self.columns), self.current_player_value)
-        self.featuremap[:, :, 1] = active_arr
+        active_arr = np.full((self.rows + self._extra_toprows, self.columns), self.current_player_value)
+        self.featuremap[:, :, layer] = active_arr
 
     def _feature_space_free_space(self):
         # layer 2 = free space
+        layer = 2
         self.featuremap_dict['2'] = "free space"
-        for row in reversed(self.featuremap):
+        for id_row, row in enumerate(reversed(self.featuremap)):
             for col in row:
-                if col[0] == 0:
-                    col[2] = 1
+                if col[0] == 0 and id_row < self.rows:
+                    col[layer] = 1
                 else:
-                    col[2] = 0
+                    col[layer] = 0
 
     def _feature_space_next_move(self):
         # layer 3 = next move space
+        layer = 3
         self.featuremap_dict['3'] = "next move space"
-        col_full = np.full((7), False)
-        for row in reversed(self.featuremap):
+
+        col_flag = np.full((self.columns), False)
+        for id_row, row in enumerate(reversed(self.featuremap)):
             for idx, col in enumerate(row):
-                if col[0] == 0 and not col_full[idx]:
-                    col[3] = 1
-                    col_full[idx] = True
+                if col[0] == 0 and not col_flag[idx] and id_row < self.rows:  # value is zero,  column is not highlighted yet, and rows is not beyond max height
+                    col[layer] = 1
+                    col_flag[idx] = True
                 else:
-                    col[3] = 0
+                    col[layer] = 0
 
     def print_feature_space(self):
         print("layer 0 = the playingField:")
@@ -263,10 +269,66 @@ class FiarGame:
         # print (f"x: {x} >> countsame: {count+1} >> win: {win} >> winner: {winner}")
         return winner  # sum(x)
 
+    def _xd_to_2d(self, state, channel=0):
+        """convert an array to a 2d array by removing anything but the given channel (default = 0)"""
+
+        dims = len(state.shape)
+
+        if dims == 2:
+            return state
+        elif dims == 3:
+            # remove dimension in order to proceed
+            return state[:, :, channel]
+        else:
+            logging.error("passing in a wrong dimension array")
+            raise Exception("passing in a wrong dimension array")
+
+    def _get_column_stackheight(self, column, state):
+        """get the index of the highest coin in the action-column\n
+        input: column and (2d) state\n
+        output: height of the stack at the given column"""
+
+        # convert state to 2d (if not already)
+        state = self._xd_to_2d(state)
+
+        # get column with values
+        column_arr = state[:, int(column)]
+
+        # return stackheight
+        col_height = len(column_arr)
+        for i, val in enumerate(column_arr):
+            if val != 0:
+                stack_height = col_height - i
+                # print(f"stackheight of column {column} ({column_arr}) is {stack_height}")
+                return stack_height
+        return 0
+
+    def _check_invalid_action_type(self, action):
+        """checking whether or not an action is invalid, in terms of types etc.\n
+        so no floats and strings etc, but an integer.\n
+        And if the action is not out of bounds. \n
+        ! Does not check on actionspace! (as this is an important value to train with)\n
+        return true: invalid action type\n
+        return false: no invalid action type """
+
+        # check for not being a integer
+        try:
+            action = int(action)
+        except Exception:
+            print(f"'{action}' is no integer, try again")
+            return True
+
+        # check for out of bounds
+        if action >= self.columns or action < 0:
+            print(f"'{action}' is out of bounds, try again")
+            return True
+        else:
+            return False
+
     def addCoin(self, inColumn, coin_value):
         # print (f"adding {coin_value} coin in column {inColumn}")
 
-        try:
+        """try:
             inColumn = int(inColumn)
         except Exception:
             print(f"'{inColumn}' is no integer, try again")
@@ -276,8 +338,40 @@ class FiarGame:
             self._invalid_move_played = True
             print(f"'{inColumn}' is out of bounds, try again")
             return False
+            """
+        if self._check_invalid_action_type(inColumn):
+            # invalid action played
+            return False
 
-        self._invalid_move_action = False
+        # convert to int
+        inColumn = int(inColumn)
+
+        # get stackheight at the given column
+        stackheight = self._get_column_stackheight(inColumn, self.playingField[:, :, 0])
+        if stackheight >= self.rows:
+            # print(f"column {inColumn} is already totally filled")
+            self._invalid_move_played = True
+            self._invalid_move_count += 1
+            self._invalid_move_action = inColumn
+            self.prev_invalid_move_reset = False
+            return False
+        else:
+            # place coin in the right spot
+            nb_rows, _ = self._xd_to_2d(self.playingField).shape
+            row = nb_rows - stackheight - 1
+            self.playingField[row, inColumn] = coin_value
+
+            # set all flags correct
+            self._invalid_move_played = False
+            self._invalid_move_action = None
+            self.turns += 1  # iterate the number of turns
+            self.prev_invalid_move_count = self._invalid_move_count  # log previous invalid move count
+            self.prev_invalid_move_reset = True  # set flag to being read
+            self._invalid_move_count = 0  # reset invalid move counter to zero
+            return True
+
+        """inColumn = int(inColumn)
+        self._invalid_move_action = None
         i = 1
         while True:
             try:
@@ -300,7 +394,29 @@ class FiarGame:
                 self._invalid_move_count += 1
                 self._invalid_move_action = inColumn
                 self.prev_invalid_move_reset = False
-                return False
+                return False"""
+
+    def make_invalid_state(self, action, state):
+        """Get the invalid state of an action. Can be used to train with"""
+        # print(state[:, :, 0])
+
+        if self._extra_toprows == 1:
+            state[0, action, 0] = self.active_player.value
+            # print(state[:, :, 0])
+            return state
+        else:
+            return state
+
+    def _get_field_no_toprows(self, state):
+        """returns the field without extra toprows (self._extra_toprows)"""
+
+        # convert state array to 2d
+        state = self._xd_to_2d(state)
+
+        # get height
+        r, _ = state.shape
+
+        return state[(r - self.rows):, :]
 
     def ShowField(self):
         print(" |0|1|2|3|4|5|6| << colums")
@@ -310,15 +426,17 @@ class FiarGame:
         if field is None:
             field = self.playingField
 
+        field = self._get_field_no_toprows(field)
+
         print("|0|1|2|3|4|5|6| << colums")
         for i in field:
             row = ""
             for j in i:
-                if j[0] == 0:
+                if j == 0:
                     coin_collor = " "
-                elif j[0] == self.player1.value:
+                elif j == self.player1.value:
                     coin_collor = self.player1.color
-                elif j[0] == self.player2.value:
+                elif j == self.player2.value:
                     coin_collor = self.player2.color
                 row += "|" + str(coin_collor)
             row += "|"
