7e1c526390634e8e90d4f99fa13095b155d52bcb
Hungryarie <arnoudzwakenberg@gmail.com>
2019-12-09 10:53:49 +0100

    #9 - split add_player_info() into two parts  1: player info -> stays add_player_info()  2: model info -> add_modelinfo() - made new method: get_info() to easaly retrieve data from the dictionaries. - changes write_parameters_to_file() and write_to_csv() to work with this new w.o.w. (dictionaries).

--- logandstats.py
+++ logandstats.py
@@ -53,15 +53,48 @@ class ModelLog():
         self.logfilepath = file
         self.log_flag = log_flag
 
-    def add_player_info(self, p1, p2):
+        self.timestamp = int(time.time())
+        self.timenow = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        self.player_dict = []   # list of dicts.
+        self.model_dict = []    # list of dicts.
+
+    def add_player_info(self, player):
+        """add player info to log\n\n
+        input:\n
+         - player -> instance of player\n
+        output:\n
+        - self.player_dict"""
+        p_dict = {'player_id': player.player_id}
+        p_dict['player_class'] = player.player_class
+        p_dict['player_name'] = player.name
+        self.player_dict.append(p_dict)
+
+    def add_modelinfo(self, player_id, model, model_info):
+        """add model info to dictionary\n\n
+        input: 
+         - player_id -> to assign the model to the correct player\n
+         - model -> model where the info is distilled from.\n
+         - model_info -> extra info about the model\n
+         output:\n
+          - self.model_dict is filled"""
+
+        dic = {}
+        dic['player_id'] = player_id
+        dic['model_info'] = model_info
+        dic['model_loss'] = model.loss
+        dic['model_acc'] = model.metrics
+        dic.update(model.hyper_dict)  # extent dic with model hyper dictionary (eg. name, class, timestamp, optimizer name, learning rate, final activation layertype etc.)
+        dic['model_fullname'] = f'{dic["model_class"]}_{dic["model_name"]}_startstamp{dic["timestamp"]}'
+        self.model_dict.append(dic)
+
+    def add_player_info_old(self, p1, p2):
+        raise DeprecationWarning("use add_player_info instead")
         self.player1_class = p1.player_class
         self.player2_class = p2.player_class
 
         self.player1_name = p1.name
         self.player2_name = p2.name
-
-        self.timestamp = int(time.time())
-        self.timenow = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
         try:
             self.model1_name = p1.model.model_name
@@ -115,33 +148,15 @@ class ModelLog():
             self.model2_used_path = 'n/a'
 
     def add_constants(self, parameters):
-        """parameters = object with attributes of trainingparameters"""
+        """add the constants parameters object to self.constants\n\n
+        input:\n
+         - parameters = object with attributes of trainingparameters"""
+
         members = [attr for attr in dir(parameters) if not callable(getattr(parameters, attr)) and not attr.startswith("__")]
         # print(members)
         self.constants = {}
         for member in members:
             self.constants[member] = getattr(parameters, member)
-
-        """self.constants = {
-            "MIN_REWARD": MIN_REWARD,
-            "EPISODES": EPISODES,
-            "REWARD_WINNING": FiarGame.REWARD_WINNING,
-            "REWARD_LOSING": FiarGame.REWARD_LOSING,
-            "REWARD_TIE": FiarGame.REWARD_TIE,
-            "REWARD_INVALID_MOVE": FiarGame.REWARD_INVALID_MOVE,
-            "REWARD_STEP": FiarGame.REWARD_STEP,
-            "DISCOUNT": DISCOUNT,
-            "REPLAY_MEMORY_SIZE": REPLAY_MEMORY_SIZE,
-            "MIN_REPLAY_MEMORY_SIZE": MIN_REPLAY_MEMORY_SIZE,
-            "MINIBATCH_SIZE": MINIBATCH_SIZE,
-            "UPDATE_TARGET_EVERY": UPDATE_TARGET_EVERY,
-            "MEMORY_FRACTION": MEMORY_FRACTION,
-            "epsilon": epsilon,
-            "EPSILON_DECAY": EPSILON_DECAY,
-            "MIN_EPSILON": MIN_EPSILON,
-            "AGGREGATE_STATS_EVERY": AGGREGATE_STATS_EVERY,
-            "SHOW_PREVIEW": SHOW_PREVIEW
-        }"""
 
     def write_parameters_to_file(self):
         if self.log_flag is False:
@@ -151,8 +166,8 @@ class ModelLog():
 
         f.write(f"=================================================\n\n")
         f.write(f"loaded models at = {self.timenow}\n")
-        f.write(f" timestamp = {self.timestamp}\n")
-
+        f.write(f" timestamp = {self.timestamp}\n\n")
+        """
         # player/model info
         f.write(f"player1\n")
         f.write(f" class = {self.player1_class}\n")
@@ -177,11 +192,27 @@ class ModelLog():
         f.write(f" model startstamp = {self.model2_timestamp}\n")
         f.write(f" model fullname = {self.model2_fullname}\n")
         f.write(f" model used path = {self.model2_used_path}\n")
+        """
+        # player en modelinfo
+        for idx, dic in enumerate(self.player_dict):
+            player_id = 0
+            f.write(f"Player {idx + 1}:\n")
+            for k, v in dic.items():
+                f.write(f" {k} = {v}\n")
+                if k == 'player_id':
+                    player_id = v
+
+            for m_dict in self.model_dict:
+                if m_dict['player_id'] == player_id:
+                    f.write(f"  model:\n")
+                    for k, v in m_dict.items():
+                        f.write(f"   {k} = {v}\n")
+            f.write("\n")
 
         # constantsinfo
         for key, constant in self.constants.items():
             if isinstance(constant, dict):
-                # constant is a dictionary
+                # if constant is a dictionary
                 f.write(f"{key}:\n")
                 for k, v in constant.items():
                     f.write(f" {k} = {v}\n")
@@ -191,6 +222,21 @@ class ModelLog():
 
         # closing file
         f.close()
+
+    def get_info(self, player_id, attribute):
+        """get the info from the self.model_dict or self.player_dict\n\n
+        when attribute can't be fount. n/a is returned"""
+
+        default = 'n/a'
+        dict_list = [self.model_dict, self.player_dict]
+        for lists in dict_list:
+            for dic in lists:
+                if dic['player_id'] == player_id:
+                    value = dic.get(attribute, default)
+                    if value != default:
+                        return value
+
+        return default
 
     def log_text_to_file(self, text):
         if self.log_flag is False:
@@ -207,6 +253,8 @@ class ModelLog():
         with open(f'{self.logfilepath}.csv', mode='a+', newline='') as parameters_file:
             parameter_writer = csv.writer(parameters_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-            parameter_writer.writerow([self.timestamp, self.timenow, 1, self.player1_class, self.player1_name, self.model1_class, self.model1_name, self.model1_loss, self.model1_opt_name, self.model1_lr, self.model1_timestamp, self.model1_fullname, self.model1_used_path])
-            parameter_writer.writerow([self.timestamp, self.timenow, 2, self.player2_class, self.player2_name, self.model2_class, self.model2_name, self.model2_loss, self.model2_opt_name, self.model2_lr, self.model2_timestamp, self.model2_fullname, self.model2_used_path])
+            #parameter_writer.writerow([self.timestamp, self.timenow, 1, self.player1_class, self.player1_name, self.model1_class, self.model1_name, self.model1_loss, self.model1_opt_name, self.model1_lr, self.model1_timestamp, self.model1_fullname, self.model1_used_path])
+            #parameter_writer.writerow([self.timestamp, self.timenow, 2, self.player2_class, self.player2_name, self.model2_class, self.model2_name, self.model2_loss, self.model2_opt_name, self.model2_lr, self.model2_timestamp, self.model2_fullname, self.model2_used_path])
+            for i in range(1, 3):
+                parameter_writer.writerow([self.timestamp, self.timenow, i, self.get_info(i, "player_class"), self.get_info(i, "player_name"), self.get_info(i, "model_class"), self.get_info(i, "model_name"), self.get_info(i, "model_loss"), self.get_info(i, "model_opt_name"), self.get_info(i, "model_lr"), self.get_info(i, "model_timestamp"), self.get_info(i, "model_fullname"), self.get_info(i, "model_used_path")])
         parameters_file.close()
