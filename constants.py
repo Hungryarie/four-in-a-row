abb125452dff876a9ae0a8aa7a88d931b0991250
Hungryarie <arnoudzwakenberg@gmail.com>
2019-12-09 10:37:24 +0100

    small changes

--- constants.py
+++ constants.py
@@ -1,25 +1,3 @@
-"""# Constants
-DISCOUNT = 0.99
-REPLAY_MEMORY_SIZE = 10_000  # 25_000  # 50_000  # How many last steps to keep for model training
-MIN_REPLAY_MEMORY_SIZE = 1_000  # Minimum number of steps in a memory to start training
-MINIBATCH_SIZE = 64  # How many steps (samples) to use for training
-UPDATE_TARGET_EVERY = 5  # Terminal states (end of episodes)
-MIN_REWARD = -60  # 120 # For model save
-MEMORY_FRACTION = 0.20  # not so relevant yet (only gpu)
-
-# Environment settings
-EPISODES = 20_000
-
-# Exploration settings
-epsilon = 1  # not a constant, going to be decayed
-EPSILON_DECAY = 0.99975
-MIN_EPSILON = 0.001
-
-#  Stats settings
-AGGREGATE_STATS_EVERY = 50  # episodes
-SHOW_PREVIEW = False
-"""
-
 
 class TrainingParameters():
     def __init__(self):
@@ -37,13 +15,12 @@ class TrainingParameters():
         self.MAX_INVALID_MOVES = 200
 
         # Exploration settings
-        #self.epsilon = 1  # not a constant, going to be decayed
         self.EPSILON_DECAY = 0.99975
         self.MIN_EPSILON = 0.001
 
         #  Stats settings
         self.AGGREGATE_STATS_EVERY = 50  # episodes
-        self.SHOW_PREVIEW = False
+        self.SHOW_PREVIEW = True
 
         # Training reward setting
         self.reward_dict = {}
@@ -51,5 +28,4 @@ class TrainingParameters():
         self.reward_dict['lose'] = -100
         self.reward_dict['tie'] = -99
         self.reward_dict['invalid'] = -10
-        self.reward_dict['step'] = -1
-
+        self.reward_dict['step'] = -0.5
